0de30fcba51082f925b37a9b527852519fe82819
Maurizio Pillitu <maurizio@session.it>
2014-10-18 22:53:04 +0200

    added IT logic to track new changes

--- alfresco-indexer-webscripts-war/src/test/java/org/alfresco/consulting/indexer/AlfrescoIndexerIT.java
+++ alfresco-indexer-webscripts-war/src/test/java/org/alfresco/consulting/indexer/AlfrescoIndexerIT.java
@@ -1,41 +1,49 @@
 package org.alfresco.consulting.indexer;
 
+import org.alfresco.consulting.indexer.client.AlfrescoClient;
+import org.alfresco.consulting.indexer.client.AlfrescoFilters;
+import org.alfresco.consulting.indexer.client.AlfrescoResponse;
+import org.alfresco.consulting.indexer.client.WebScriptsAlfrescoClient;
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import org.alfresco.consulting.indexer.client.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.*;
+
 public class AlfrescoIndexerIT {
+
+  private static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHss");
 
   @Test
   public void fetchNodes() {
     AlfrescoClient client = new WebScriptsAlfrescoClient(
-        "http","localhost:8080",
+        "http", "localhost:8080",
         "/alfresco/service",
         "workspace",
         "SpacesStore",
-        "admin","admin");
+        "admin", "admin");
     assertNotNull(client);
 
+    //Fetching all Alfresco nodes, expecting 65 results
     AlfrescoResponse response = client.fetchNodes(0, 0, new AlfrescoFilters());
     List<Map<String, Object>> list = response.getDocumentList();
-
     assertEquals(65, list.size());
 
+    //Checking results
     assertEquals("SpacesStore", response.getStoreId());
     assertEquals("workspace", response.getStoreProtocol());
 
-    for(Map<String, Object> d : list) {
-      String uuid = (String)d.get("uuid");
+    //Fetching metadata of one specific result, given its path
+    for (Map<String, Object> d : list) {
+      String uuid = (String) d.get("uuid");
       Map<String, Object> metadata = client.fetchMetadata(uuid);
-      String path = (String)metadata.get("path");
+      String path = (String) metadata.get("path");
       if ("/app:company_home/st:sites/cm:swsdp/cm:documentLibrary/cm:Agency_x0020_Files/cm:Logo_x0020_Files/cm:logo.png".equals(path)) {
-        List<String> aspects = (List<String>)metadata.get("aspects");
-        List<String> readableAuthorities = (List<String>)metadata.get("readableAuthorities");
+        List<String> aspects = (List<String>) metadata.get("aspects");
+        List<String> readableAuthorities = (List<String>) metadata.get("readableAuthorities");
 
         assertTrue(aspects.contains("cm:titled"));
         assertTrue(aspects.contains("exif:exif"));
@@ -45,15 +53,32 @@ public class AlfrescoIndexerIT {
         assertTrue(readableAuthorities.contains("GROUP_site_swsdp_SiteConsumer"));
         assertTrue(readableAuthorities.contains("GROUP_site_swsdp_SiteContributor"));
 
-        assertEquals("logo.png",metadata.get("cm:title"));
-        assertEquals("abeecher",metadata.get("cm:modifier"));
+        assertEquals("logo.png", metadata.get("cm:title"));
+        assertEquals("abeecher", metadata.get("cm:modifier"));
 
-        assertTrue(((String)metadata.get("documentUrl")).contains(uuid));
-        assertTrue(((String)metadata.get("contentUrlPath")).contains(uuid));
-        assertTrue(((String)metadata.get("shareUrlPath")).contains(uuid));
-        assertTrue(((String)metadata.get("thumbnailUrlPath")).contains(uuid));
-        assertTrue(((String)metadata.get("previewUrlPath")).contains(uuid));
-     }
+        assertTrue(((String) metadata.get("documentUrl")).contains(uuid));
+        assertTrue(((String) metadata.get("contentUrlPath")).contains(uuid));
+        assertTrue(((String) metadata.get("shareUrlPath")).contains(uuid));
+        assertTrue(((String) metadata.get("thumbnailUrlPath")).contains(uuid));
+        assertTrue(((String) metadata.get("previewUrlPath")).contains(uuid));
+      }
     }
+
+    //Adding a document in Alfresco via CMIS
+    CMISUtils cdc = new CMISUtils();
+    cdc.setUser("admin");
+    cdc.setPassword("admin");
+    cdc.createDocument("test" + "." + sdf.format(new Date()), "cmis:document");
+
+    //Fetching nodes again, starting from latest transaction and ACL changeset IDs
+    response = client.fetchNodes(response.getLastTransactionId(), response.getLastAclChangesetId(), new AlfrescoFilters());
+    list = response.getDocumentList();
+    assertEquals(1, list.size());
+
+    //Checking metadata
+    String uuid = (String) list.get(0).get("uuid");
+    Map<String, Object> metadata = client.fetchMetadata(uuid);
+    assertTrue(((String) metadata.get("cm:name")).startsWith("test"));
+    assertEquals("admin", metadata.get("cm:modifier"));
   }
 }
