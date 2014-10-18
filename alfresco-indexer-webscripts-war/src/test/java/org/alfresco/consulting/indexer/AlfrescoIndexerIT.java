0deee8d90a7730ce2ea2510af91ce7a29289f39c
Maurizio Pillitu <maurizio@session.it>
2014-10-18 20:21:32 +0200

    cleaned up code

--- alfresco-indexer-webscripts-war/src/test/java/org/alfresco/consulting/indexer/AlfrescoIndexerIT.java
+++ alfresco-indexer-webscripts-war/src/test/java/org/alfresco/consulting/indexer/AlfrescoIndexerIT.java
@@ -26,42 +26,34 @@ public class AlfrescoIndexerIT {
 
     assertEquals(65, list.size());
 
-    Map<String, Object> doc = list.get(0);
     assertEquals("SpacesStore", response.getStoreId());
     assertEquals("workspace", response.getStoreProtocol());
 
     for(Map<String, Object> d : list) {
-      String type = (String)d.get("type");
       String uuid = (String)d.get("uuid");
-      Boolean deleted = (Boolean)d.get("deleted");
-      if ("cm:content".equals(type) && deleted == false) {
-        Map<String, Object> metadata = client.fetchMetadata(uuid);
-        String path = (String)metadata.get("path");
-        if ("/app:company_home/st:sites/cm:swsdp/cm:documentLibrary/cm:Agency_x0020_Files/cm:Logo_x0020_Files/cm:logo.png".equals(path)) {
-          List<String> aspects = (List<String>)metadata.get("aspects");
-          List<String> readableAuthorities = (List<String>)metadata.get("readableAuthorities");
+      Map<String, Object> metadata = client.fetchMetadata(uuid);
+      String path = (String)metadata.get("path");
+      if ("/app:company_home/st:sites/cm:swsdp/cm:documentLibrary/cm:Agency_x0020_Files/cm:Logo_x0020_Files/cm:logo.png".equals(path)) {
+        List<String> aspects = (List<String>)metadata.get("aspects");
+        List<String> readableAuthorities = (List<String>)metadata.get("readableAuthorities");
 
-          assertTrue(aspects.contains("cm:titled"));
-          assertTrue(aspects.contains("exif:exif"));
-          assertTrue(aspects.contains("cm:rateable"));
+        assertTrue(aspects.contains("cm:titled"));
+        assertTrue(aspects.contains("exif:exif"));
+        assertTrue(aspects.contains("cm:rateable"));
 
-          assertTrue(readableAuthorities.contains("GROUP_EVERYONE"));
-          assertTrue(readableAuthorities.contains("GROUP_site_swsdp_SiteConsumer"));
-          assertTrue(readableAuthorities.contains("GROUP_site_swsdp_SiteContributor"));
+        assertTrue(readableAuthorities.contains("GROUP_EVERYONE"));
+        assertTrue(readableAuthorities.contains("GROUP_site_swsdp_SiteConsumer"));
+        assertTrue(readableAuthorities.contains("GROUP_site_swsdp_SiteContributor"));
 
-          assertEquals("logo.png",metadata.get("cm:title"));
-          assertEquals("abeecher",metadata.get("cm:modifier"));
+        assertEquals("logo.png",metadata.get("cm:title"));
+        assertEquals("abeecher",metadata.get("cm:modifier"));
 
-          assertTrue(((String)metadata.get("documentUrl")).contains(uuid));
-          assertTrue(((String)metadata.get("contentUrlPath")).contains(uuid));
-          assertTrue(((String)metadata.get("shareUrlPath")).contains(uuid));
-          assertTrue(((String)metadata.get("thumbnailUrlPath")).contains(uuid));
-          assertTrue(((String)metadata.get("previewUrlPath")).contains(uuid));
-
-          //System.out.println(metadata);
-
-        }
-      }
+        assertTrue(((String)metadata.get("documentUrl")).contains(uuid));
+        assertTrue(((String)metadata.get("contentUrlPath")).contains(uuid));
+        assertTrue(((String)metadata.get("shareUrlPath")).contains(uuid));
+        assertTrue(((String)metadata.get("thumbnailUrlPath")).contains(uuid));
+        assertTrue(((String)metadata.get("previewUrlPath")).contains(uuid));
+     }
     }
   }
 }
